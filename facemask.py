118d41e828638ce9845f9b56628ddd2cb99ec5ed
Karan Malik <42814324+Karan-Malik@users.noreply.github.com>
2020-09-17 18:55:15 +0530

    Added Detection for multiple faces

--- facemask.py
+++ facemask.py
@@ -77,24 +77,28 @@ type(mymodel.predict_classes(test_image)[0][0])
 
 # IMPLEMENTING LIVE DETECTION OF FACE MASK
 
-mymodel=load_model('mymodel.h5')
-
 cap=cv2.VideoCapture(0)
+face_cascade=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 while cap.isOpened():
     _,img=cap.read()
-    face_img=cv2.resize(img,(150,150))
-    face_img=face_img/255.0
-    face_img=np.reshape(face_img,(150,150,3))
-    face_img=np.expand_dims(face_img,axis=0)
-    pred=mymodel.predict_classes(face_img)
-    if pred[0][0]==1:
-        cv2.putText(img,'NO MASK',(250,50),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),4)
-    else:
-        cv2.putText(img,'MASK',(250,50),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),4)
-    datet=str(datetime.datetime.now())
-    cv2.putText(img,datet,(400,450),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,255,255),1)
-        
+    face=face_cascade.detectMultiScale(img,scaleFactor=1.1,minNeighbors=4)
+    for(x,y,w,h) in face:
+        face_img = img[y:y+h, x:x+w]
+        cv2.imwrite('temp.jpg',face_img)
+        test_image=image.load_img('temp.jpg',target_size=(150,150,3))
+        test_image=image.img_to_array(test_image)
+        test_image=np.expand_dims(test_image,axis=0)
+        pred=mymodel.predict_classes(test_image)[0][0]
+        if pred==1:
+            cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),3)
+            cv2.putText(img,'NO MASK',((x+w)//2,y+h+20),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),3)
+        else:
+            cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),3)
+            cv2.putText(img,'MASK',((x+w)//2,y+h+20),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),3)
+        datet=str(datetime.datetime.now())
+        cv2.putText(img,datet,(400,450),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,255,255),1)
+          
     cv2.imshow('img',img)
     
     if cv2.waitKey(1)==ord('q'):
@@ -102,4 +106,3 @@ while cap.isOpened():
     
 cap.release()
 cv2.destroyAllWindows()
-   
