331cd64526029ff93268f3f8860e989cd276129b
subhankar biswas <sub.nitt@yahoo.com>
2021-05-05 20:51:44 +0530

    my commit

--- facemask.py
+++ facemask.py
@@ -20,61 +20,60 @@ import datetime
 # UNCOMMENT THE FOLLOWING CODE TO TRAIN THE CNN FROM SCRATCH
 
 # BUILDING MODEL TO CLASSIFY BETWEEN MASK AND NO MASK
-'''
-model=Sequential()
-model.add(Conv2D(32,(3,3),activation='relu',input_shape=(150,150,3)))
-model.add(MaxPooling2D() )
-model.add(Conv2D(32,(3,3),activation='relu'))
-model.add(MaxPooling2D() )
-model.add(Conv2D(32,(3,3),activation='relu'))
-model.add(MaxPooling2D() )
-model.add(Flatten())
-model.add(Dense(100,activation='relu'))
-model.add(Dense(1,activation='sigmoid'))
+# model=Sequential()
+# model.add(Conv2D(32,(3,3),activation='relu',input_shape=(150,150,3)))
+# model.add(MaxPooling2D() )
+# model.add(Conv2D(32,(3,3),activation='relu'))
+# model.add(MaxPooling2D() )
+# model.add(Conv2D(32,(3,3),activation='relu'))
+# model.add(MaxPooling2D() )
+# model.add(Flatten())
+# model.add(Dense(100,activation='relu'))
+# model.add(Dense(1,activation='sigmoid'))
 
-model.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])
+# model.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])
 
-from keras.preprocessing.image import ImageDataGenerator
-train_datagen = ImageDataGenerator(
-        rescale=1./255,
-        shear_range=0.2,
-        zoom_range=0.2,
-        horizontal_flip=True)
+# from keras.preprocessing.image import ImageDataGenerator
+# train_datagen = ImageDataGenerator(
+#         rescale=1./255,
+#         shear_range=0.2,
+#         zoom_range=0.2,
+#         horizontal_flip=True)
 
-test_datagen = ImageDataGenerator(rescale=1./255)
+# test_datagen = ImageDataGenerator(rescale=1./255)
 
-training_set = train_datagen.flow_from_directory(
-        'train',
-        target_size=(150,150),
-        batch_size=16 ,
-        class_mode='binary')
+# training_set = train_datagen.flow_from_directory(
+#         'train',
+#         target_size=(150,150),
+#         batch_size=16 ,
+#         class_mode='binary')
 
-test_set = test_datagen.flow_from_directory(
-        'test',
-        target_size=(150,150),
-        batch_size=16,
-        class_mode='binary')
+# test_set = test_datagen.flow_from_directory(
+#         'test',
+#         target_size=(150,150),
+#         batch_size=16,
+#         class_mode='binary')
 
-model_saved=model.fit_generator(
-        training_set,
-        epochs=10,
-        validation_data=test_set,
+# model_saved=model.fit_generator(
+#         training_set,
+#         epochs=10,
+#         validation_data=test_set,
 
-        )
+#         )
 
-model.save('mymodel.h5',model_saved)
+# model.save('mymodel.h5',model_saved)
 
-#To test for individual images
+# #To test for individual images
 
-mymodel=load_model('mymodel.h5')
-#test_image=image.load_img('C:/Users/Karan/Desktop/ML Datasets/Face Mask Detection/Dataset/test/without_mask/30.jpg',target_size=(150,150,3))
-test_image=image.load_img(r'C:\Users\Karan\Pictures\Camera Roll/21.jpg',
-                          target_size=(150,150,3))
-test_image
-test_image=image.img_to_array(test_image)
-test_image=np.expand_dims(test_image,axis=0)
-type(mymodel.predict_classes(test_image)[0][0])
-'''
+# mymodel=load_model('mymodel.h5')
+# #test_image=image.load_img('C:/Users/Karan/Desktop/ML Datasets/Face Mask Detection/Dataset/test/without_mask/30.jpg',target_size=(150,150,3))
+# test_image=image.load_img(r'C:\Users\Karan\Pictures\Camera Roll/21.jpg',
+#                           target_size=(150,150,3))
+# test_image
+# test_image=image.img_to_array(test_image)
+# test_image=np.expand_dims(test_image,axis=0)
+# type(mymodel.predict_classes(test_image)[0][0])
+
 
 # IMPLEMENTING LIVE DETECTION OF FACE MASK
 
